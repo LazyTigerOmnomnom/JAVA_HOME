da0d85c663715bf17fe948fa2c502d8a5bd9ca1c
LazyTigerOmnomnom <63197649+LazyTigerOmnomnom@users.noreply.github.com>
2022-10-27 20:03:34 +0300

    Add files via upload
    Добавлено решение 3ей задачи

--- HomeWork3/Task3/Main.java
+++ HomeWork3/Task3/Main.java
@@ -1,4 +1,20 @@
 package HomeWork3.Task3;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+        int num = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+        for(int i = num; i <= num2; i++){
+            if (i % num3 == 0){\
+                System.out.print(i);
+                System.out.print(" ");
+            }
+        }
+    }
+
 }
